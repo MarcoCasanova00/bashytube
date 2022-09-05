53db1d60952bde6753be18d88ef2550b78ca0fa7
Marco <94206717+MarcoCasanova00@users.noreply.github.com>
2022-09-05 04:07:18 +0200

    Add files via upload

--- bashytube_1_0.sh
+++ bashytube_1_0.sh
@@ -13,27 +13,27 @@ echo "                             ";
 
 # prompt for youtube link and use youtube-dl to download related mp3 file to present directory
 
-echo please, enter youtube URL to download the mp3 from: 
+echo "Enter youtube URL to download the mp3 from: \n"
 read varurl
 
 sleep 1
-echo downloading $varurl ... please wait ...
+echo "Downloading $varurl ... Please wait ..."
 sleep 1
 youtube-dl -x --audio-format mp3 -f bestaudio -o '%(view_count)s.%(ext)s' $varurl
 sleep 1
-echo probably done, go check the directory this script was run from.
+echo "Probably done, go check the directory this script was run from.\n"
 
 
 # takes random image from provided image_folder and fetches previously downloaded mp3 to combine it with image using ffmpeg
 # check the extension and the dir
 
 
-echo now please enter the full name of the mp3 you wish to use: 
+echo "Now please enter the full name of the mp3 you wish to use: \n"
 ls | grep ".mp3$"
 read MP3
 
 sleep 1
-echo reading file ...
+echo "Reading file ... \n"
 
 # for the video :
 
@@ -43,6 +43,6 @@ ffmpeg -r 1 -loop 1 -i $IMG -i $MP3 -acodec copy -r 1 -shortest -vf scale=1280:7
 
 
 sleep 1
-echo probably done. goodbye!
+echo "Probably done. goodbye! \n"
 
 
