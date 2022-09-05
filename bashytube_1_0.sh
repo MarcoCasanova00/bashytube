c6c5c2e5c7972f8d5957769109f42bdd56f99704
Marco <94206717+MarcoCasanova00@users.noreply.github.com>
2022-09-05 04:09:00 +0200

    Add files via upload

--- bashytube_1_0.sh
+++ bashytube_1_0.sh
@@ -10,30 +10,32 @@ echo " |_   _|_  _ | |__  ___  _ _ ";
 echo "   | | | || || '_ \/ -_)| '_|";
 echo "   |_|  \_,_||_.__/\___||_|  ";
 echo "                             ";
+echo BashyTuber 1.0 by Marco Casanova
 
 # prompt for youtube link and use youtube-dl to download related mp3 file to present directory
-
-echo "Enter youtube URL to download the mp3 from: \n"
+UYEL='\033[4;33m'
+NC='\033[0m'
+echo -e  "${UYEL}Enter youtube URL to download the mp3 from:${NC}"
 read varurl
 
 sleep 1
-echo "Downloading $varurl ... Please wait ..."
+echo -e "${UYEL}Downloading $varurl ... Please wait ...${NC}"
 sleep 1
 youtube-dl -x --audio-format mp3 -f bestaudio -o '%(view_count)s.%(ext)s' $varurl
 sleep 1
-echo "Probably done, go check the directory this script was run from.\n"
+echo -e "${UYEL}Probably done, go check the directory this script was run from.\n${NC}"
 
 
 # takes random image from provided image_folder and fetches previously downloaded mp3 to combine it with image using ffmpeg
 # check the extension and the dir
 
 
-echo "Now please enter the full name of the mp3 you wish to use: \n"
+echo -e "${UYEL}Enter the full name of the mp3 you wish to use: (Output will be empty if mp3 absent from directory.)${NC}"
 ls | grep ".mp3$"
 read MP3
 
 sleep 1
-echo "Reading file ... \n"
+echo -e "${UYEL}Reading file ...${NC}"
 
 # for the video :
 
@@ -43,6 +45,6 @@ ffmpeg -r 1 -loop 1 -i $IMG -i $MP3 -acodec copy -r 1 -shortest -vf scale=1280:7
 
 
 sleep 1
-echo "Probably done. goodbye! \n"
+echo -e "${UYEL}Probably done. goodbye!${NC}"
 
 
